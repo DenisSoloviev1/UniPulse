004b798ba1429fe8b465307c3dd58eb109fd3914
Denis <solovievdenis2004@gmail.com>
2024-11-29 15:21:37 +0300

    add select tg

--- src/pages/AddChannel/index.tsx
+++ src/pages/AddChannel/index.tsx
@@ -1,61 +1,48 @@
 import React, { useEffect, useState } from "react";
-import {addTelegramChannel} from "../../entities/user";
+import { addTelegramChannel } from "../../entities/user";
 import { useNavigate } from "react-router-dom";
-import {useAuthStore} from "../../entities/auth";
-
+import { useAuthStore } from "../../entities/auth";
+import { Wrapper } from "../style";
+import { Loader } from "../../shared/ui";
 
 export const AddChannel: React.FC = () => {
-    const navigate = useNavigate();
-    const { isAuth, userId, setUserId } = useAuthStore();
+  const navigate = useNavigate();
+  const { isAuth, setUserId } = useAuthStore();
 
-    const [channelId, setChannelId] = useState<string | null>(null);
+  const [channelId, setChannelId] = useState<string | null>(null);
 
-    useEffect(() => {
-        console.log("qweqweqweqweqewqwe")
-        // Получаем текущий URL
-        const url = new URL(window.location.href);
-        // Извлекаем параметр id
-        const id = url.searchParams.get("id");
-        setChannelId(id);
+  useEffect(() => {
+    // Получаем текущий URL
+    const url = new URL(window.location.href);
+    // Извлекаем параметр id
+    const id = url.searchParams.get("id");
+    setChannelId(id);
 
-        console.log('isAuth', isAuth)
+    console.log("isAuth", isAuth);
 
-        if (!isAuth) {
+    if (!isAuth) {
+      if (id != null) {
+        setUserId(id);
+      }
+      navigate("/");
+    } else {
+      void addTelegram();
+    }
 
-            if (id != null) {
-                console.log('donme')
-                setUserId(id)
-            }
-            navigate('/');
-        } else {
-            void addTelegram()
+    async function addTelegram() {
+      try {
+        const result = await addTelegramChannel(id);
+
+        console.log("Успех:", result);
+        if (result.success) {
+          // Если сервер вернул положительный ответ
+          navigate("/myNotif");
         }
+      } catch (error) {
+        console.error("Ошибка при добавлении канала:", error);
+      }
+    }
+  }, []);
 
-        async function addTelegram(){
-            try {
-                const result = await addTelegramChannel(id);
-
-                console.log('Успех:', result);
-                if (result.success) {
-                    // Если сервер вернул положительный ответ
-                    navigate("/myNotif")
-                }
-            } catch (error) {
-                console.error('Ошибка при добавлении канала:', error);
-            }
-        }
-        // Сохраняем в состояние
-
-    }, []);
-
-    return (
-        <div>
-            <h1>Add Channel</h1>
-            {channelId ? (
-                <p>Channel ID: {channelId}</p>
-            ) : (
-                <p>No Channel ID found</p>
-            )}
-        </div>
-    );
+  return <Loader $size={"200px"} />;
 };
