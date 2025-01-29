898db661a022a0c5d6c6b90acdf78111d55ccf5c
Denis <solovievdenis2004@gmail.com>
2025-01-29 08:44:15 +0300

    fixed padding and callback

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,20 +3,9 @@ import { Loader } from "../shared/ui";
 import { BrowserRouter } from "react-router-dom";
 import { Routing } from "../pages/lib/guards";
 import "./styles.scss";
-import { useFetchRole } from "../shared/hooks/useFetchRole";
 import { ToastContainer } from "react-toastify";
 
 const App = () => {
-  const { isLoading, isError } = useFetchRole();
-
-  if (isLoading) {
-    return <Loader size={"200px"} color={"blue"} />;
-  }
-
-  if (isError) {
-    return <p>Извините что-то пошло не так</p>;
-  }
-
   return (
     <BrowserRouter>
       <Suspense fallback={<Loader size={"200px"} color={"blue"} />}>
