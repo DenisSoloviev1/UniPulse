4b9c28452d1477b78c25f01b4a0a3a3b5be6436f
randepukperesoh <mirgthebest@gmail.com>
2025-01-29 10:00:52 +0300

    fix sorted

--- src/shared/hooks/useFilters.ts
+++ src/shared/hooks/useFilters.ts
@@ -7,17 +7,21 @@ export const useFilters = (arr: INotif[]) => {
     const [groupId, setGroupId] = useState(1);
     const [sort, setSort] = useState<sortType>('desc'); 
     const [filteredArr, setFilteredArr] = useState<INotif[]>([]);
+    const [initializedSort, setInitializedSort] = useState(false);
 
     useEffect(() => {
         setFilteredArr( arr.filter(el => el.tags.some(({id}) => groupId === 1 || id === groupId)));
     }, [groupId, arr]);
 
     useEffect(() => {
+        if(!initializedSort) {
+            setInitializedSort(true);
+            return
+        }
+
         setFilteredArr(prev => prev.slice().reverse());
+        
     }, [sort])
-
-
-    // const s1 = (value: sortType) => {};
 
     return { filteredArr, setGroupId, setSort };
 };
